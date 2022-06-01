9db97cc42a4f5159ad5d1661c25ced5fd23e70c3
ariel <nikitosiks338@gmail.com>
2022-06-01 21:21:28 +0300

    open-exchange-rates APi added

---
+++ src/main/kotlin/com/example/dolorcurrencyexchangeratetogif/api/OpenExchangeRatesApi.kt
@@ -0,0 +1,16 @@
+package com.example.dolorcurrencyexchangeratetogif.api
+
+import com.example.dolorcurrencyexchangeratetogif.payload.GifResponse
+import feign.Param
+import org.springframework.cloud.openfeign.FeignClient
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
+
+@FeignClient(name = "open-exchange-rates", url = "\${api.open-exchange-rates.url.base}", )
+interface OpenExchangeRatesApi {
+    @GetMapping("\${api.open-exchange-rates.url.latest}")
+    fun getLatest(): String
+
+    @GetMapping("\${api.open-exchange-rates.url.by-date}")
+    fun getByDate(@RequestParam date: String): String
+}
--- src/main/kotlin/com/example/dolorcurrencyexchangeratetogif/controller/CurrencyController.kt
+++ src/main/kotlin/com/example/dolorcurrencyexchangeratetogif/controller/CurrencyController.kt
@@ -1,19 +1,19 @@
 package com.example.dolorcurrencyexchangeratetogif.controller
 
 import com.example.dolorcurrencyexchangeratetogif.api.GiphyApi
-import com.example.dolorcurrencyexchangeratetogif.payload.GifResponse
+import com.example.dolorcurrencyexchangeratetogif.api.OpenExchangeRatesApi
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RestController
-import java.util.Currency
 
 @RestController
 class CurrencyToGifController(
-    val giphyApi: GiphyApi
+    val giphyApi: GiphyApi,
+    val openExchangeRatesApi: OpenExchangeRatesApi
 ) {
     @GetMapping("/{currency}")
-    fun getGif(@PathVariable currency: String): GifResponse {
-        return giphyApi.findRandomRich()
+    fun getGif(@PathVariable currency: String): String {
+        return openExchangeRatesApi.getByDate("2022-05-30")
         TODO("Not Implemented")
     }
 }
