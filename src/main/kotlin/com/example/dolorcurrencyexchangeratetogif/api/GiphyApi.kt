c76b62a209b1503b9fc44cb4ab058b16ec1acfc3
ariel <nikitosiks338@gmail.com>
2022-06-01 21:33:05 +0300

    warnings solved, .properties files separated

--- src/main/kotlin/com/example/dolorcurrencyexchangeratetogif/api/GiphyApi.kt
+++ src/main/kotlin/com/example/dolorcurrencyexchangeratetogif/api/GiphyApi.kt
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient
 import org.springframework.web.bind.annotation.GetMapping
 
 
-@FeignClient(name = "giphy", url = "\${api.giphy.url.base}", )
+@FeignClient(name = "giphy", url = "\${api.giphy.url.base}")
 interface GiphyApi {
 
     @GetMapping("\${api.giphy.url.rich}")
--- src/main/kotlin/com/example/dolorcurrencyexchangeratetogif/api/OpenExchangeRatesApi.kt
+++ src/main/kotlin/com/example/dolorcurrencyexchangeratetogif/api/OpenExchangeRatesApi.kt
@@ -1,12 +1,10 @@
 package com.example.dolorcurrencyexchangeratetogif.api
 
-import com.example.dolorcurrencyexchangeratetogif.payload.GifResponse
-import feign.Param
 import org.springframework.cloud.openfeign.FeignClient
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
 
-@FeignClient(name = "open-exchange-rates", url = "\${api.open-exchange-rates.url.base}", )
+@FeignClient(name = "open-exchange-rates", url = "\${api.open-exchange-rates.url.base}")
 interface OpenExchangeRatesApi {
     @GetMapping("\${api.open-exchange-rates.url.latest}")
     fun getLatest(): String
